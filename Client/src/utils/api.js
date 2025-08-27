5451e5559e339341a87aea6c362d4811ef024005
Ziad Teama <ziadbasaria600@gmail.com>
2025-08-28 01:05:16 +0300

    Implement structural updates and optimizations across multiple modules

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -1,5 +1,5 @@
-const baseUrl = 'http://192.168.1.245:3000';
-//const baseUrl = 'http://localhost:3000';
+//const baseUrl = 'http://192.168.1.245:3000';
+const baseUrl = 'http://localhost:3000';
 
 class ApiClient {
   constructor() {
